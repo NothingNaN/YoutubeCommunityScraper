5b4dcf62f2ed0940f35e426c99d694d8b32aa078
Boltzmann Potato <boltzmannpotato@proton.me>
2023-10-27 05:11:54 +0300

    Version 0.9.2
    Added a few quality of life features such as: update support for existing json files, directory path choice for download of posts, verbose support, reverse post order, edited progress bar to give a number of updated posts when using the --update argument, added utc timestamp for when each post has been downloaded.

--- ypdl.py
+++ ypdl.py
@@ -1,3 +1,4 @@
+import itertools
 from requests import Response
 from requests_html import HTMLSession, AsyncHTMLSession
 from lxml import etree
@@ -8,6 +9,7 @@ import sys
 import asyncio
 import argparse
 from rich.progress import Progress, TextColumn, TimeElapsedColumn, SpinnerColumn
+from datetime import datetime, timezone
 
 logging.basicConfig(format='[%(levelname)s]: %(message)s', level=logging.WARNING, stream=sys.stdout)
 
@@ -50,7 +52,7 @@ def _get_video_link(post: dict) -> str | None:
     try:
         return 'https://www.youtube.com/watch?v=' + post['backstageAttachment']['videoRenderer']['videoId']
     except KeyError as error:
-        logging.debug(error)
+        logging.debug(f'function: _get_video_link: {error}')
         return None
 
 
@@ -58,7 +60,7 @@ def _get_image_link(post: dict) -> str | None:
     try:
         return post['backstageAttachment']['backstageImageRenderer']['image']['thumbnails'][-1]['url']
     except KeyError as error:
-        logging.debug(error)
+        logging.debug(f'function: _get_image_link: {error}')
         return None
 
 
@@ -66,7 +68,7 @@ def _get_text(post: dict) -> str | None:
     try:
         return post['contentText']['runs'][0]['text']
     except KeyError as error:
-        logging.debug(error)
+        logging.debug(f'function: _get_text: {error}')
         return None
 
 
@@ -74,6 +76,7 @@ def _get_content(post: dict) -> dict:
     post_info = {
         "post_link": 'https://www.youtube.com/post/' + post['postId'],
         "time_since": post['publishedTimeText']['runs'][0]['text'],
+        "time_of_download": datetime.now(timezone.utc).strftime("%d/%m/%Y, %H:%M:%S"),
         "video": _get_video_link(post),
         "image": _get_image_link(post),
         "text": _get_text(post)
@@ -92,6 +95,7 @@ class YoutubePosts:
         self.link = channel_link + '/community'
         self.session = AsyncHTMLSession()
         self.cookies = cookies
+        self.taskID = None
 
     async def request(self, init: bool = True) -> Response:
         if init:
@@ -132,7 +136,6 @@ class YoutubePosts:
 
     async def scrape(self, pbar: Progress) -> None:
         response = await self.request(init=True)
-        # print(pbar_position)
         self.__get_API_key(response)
         self.__get_API_URL(response)
         self.__get_token(response)
@@ -140,7 +143,7 @@ class YoutubePosts:
 
         total = len(self.posts)
         init_total = total
-        taskID = pbar.add_task(f"{self.channel_name}", total=total)
+        self.taskID = pbar.add_task(f"{self.channel_name}", total=total, new=None)
 
         eof = False
         while not eof:
@@ -152,13 +155,13 @@ class YoutubePosts:
                 break
 
             total += len(posts) - 1
-            pbar.update(taskID, total=total)
+            pbar.update(self.taskID, total=total)
 
             for i, post in enumerate(posts):
                 if i != len(posts) - 1:
                     postRenderer = post['backstagePostThreadRenderer']['post']['backstagePostRenderer']
                     self.posts.append(_get_content(post=postRenderer))
-                    pbar.update(taskID, advance=1)
+                    pbar.update(self.taskID, advance=1)
                 else:
                     try:
                         self.token = post['continuationItemRenderer']['continuationEndpoint']['continuationCommand']['token']
@@ -167,32 +170,85 @@ class YoutubePosts:
                         postRenderer = post['backstagePostThreadRenderer']['post']['backstagePostRenderer']
                         self.posts.append(_get_content(post=postRenderer))
                         total += 1
-                        pbar.update(taskID, advance=1, total=total)
+                        pbar.update(self.taskID, advance=1, total=total)
 
         # this is here for the spinner to keep spinning without interruptions
-        pbar.update(taskID, advance=init_total)
+        pbar.update(self.taskID, advance=init_total)
+        self.posts.reverse()  # reverses post order from newest first to oldest first
 
-    def save(self) -> None:
-        with open(f'{self.channel_name}_posts.json', 'w', encoding='utf-8') as target:
+    def save(self, pbar: Progress, folder: str | None = None, reverse: bool = False, update: bool = False) -> None:
+
+        if folder:
+            folder += '/' if folder[-1] != '/' else None
+        if reverse:
+            self.posts.reverse()
+        if update:
+            with open(f'{folder}{self.channel_name}_posts.json', 'r', encoding='utf-8') as target:
+                oldposts = json.load(target)
+                oldpostIDs = {post['post_link']: 'post_link' for post in oldposts}
+                i = 0
+                for post in self.posts[:]:
+                    try:
+                        oldpostIDs[post['post_link']]
+                        self.posts.pop(i)
+                    except KeyError:
+                        i += 1
+
+                pbar.update(self.taskID, new=len(self.posts))
+                self.posts = list(itertools.chain(oldposts, self.posts))
+
+        with open(f'{folder}{self.channel_name}_posts.json', 'w', encoding='utf-8') as target:
             json.dump(self.posts, target, ensure_ascii=False, indent=4)
 
 
+def get_arg_parser():
+    ap = argparse.ArgumentParser(prog='yp-dl',
+                                 description='An asynchronous scraper that downloads youtube posts from youtube channels in json format.')
+    ap.add_argument('link', nargs='+',
+                    help='Provide any number of links. Link example: https://www.youtube.com/@3blue1brown')
+    ap.add_argument('-f', '--folder-path', dest='folderpath', metavar='FOLDER_PATH', type=str, default='',
+                    help='Provide the path of the folder you wish to store/update your json files. If it\'s in the current working directory (CWD), just type the folder name. If none is provided, everything will be stored/updated in the CWD.')
+    ap.add_argument('-r', '--reverse', action='store_true',
+                    help='Reverses the order of the posts from oldest first to newest first. Be wary though, if you use this option with --update, your post order will be messed up.')
+    ap.add_argument('-u', '--update', action='store_true',
+                    help='Appends the existing json file(s) with the new posts.')
+    ap.add_argument('-v', '--verbose', action='store_true',
+                    help='Gives more details about what\'s going on when the program runs.')
+
+    return ap
+
+
+def get_pbar(update: bool = False):
+    if update:
+        pbar = Progress(TextColumn("[progress.description]{task.description}"),
+                        SpinnerColumn(),
+                        TextColumn("[#9B59B6]{task.completed}/{task.total}"),
+                        TimeElapsedColumn(),
+                        TextColumn("[cyan]New posts: {task.fields[new]}"))
+    else:
+        pbar = Progress(TextColumn("[progress.description]{task.description}"),
+                        SpinnerColumn(),
+                        TextColumn("[#9B59B6]{task.completed}/{task.total}"),
+                        TimeElapsedColumn())
+
+    return pbar
+
+# TODO: maybe add polls? need specific cookies to unlock poll votes
+
 if __name__ == "__main__":
     cookies = {"SOCS": _get_SOCS_cookie()}
-    ap = argparse.ArgumentParser()
-    ap.add_argument('links', nargs='+')
+    ap = get_arg_parser()
     args = vars(ap.parse_args())
 
-    yt_links = args['links']
-    objects = [YoutubePosts(link, cookies) for link in yt_links]
+    if args['verbose']:
+        logging.getLogger().setLevel(logging.DEBUG)
 
-    pbar = Progress(TextColumn("[progress.description]{task.description}"),
-                    SpinnerColumn(),
-                    TextColumn("[#9B59B6]{task.completed}/{task.total}"), TimeElapsedColumn())
+    objects = [YoutubePosts(link, cookies) for link in args['link']]
+    pbar = get_pbar(args['update'])
+
     tasks = [obj.scrape(pbar) for obj in objects]
     loop = asyncio.get_event_loop()
     with pbar:
         loop.run_until_complete(asyncio.gather(*tasks))
-
-    for obj in objects:
-        obj.save()
+        for obj in objects:
+            obj.save(pbar, folder=args['folderpath'], reverse=args['reverse'], update=args['update'])
